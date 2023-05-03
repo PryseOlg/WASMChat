1e541a38e24d1b076225bd563c416b29fa4ab276
Игорь Бабин <igorbabin2002@gmail.com>
2023-05-03 14:57:26 +0500

    Removed redundant string const

--- Server/Models/ClaimsPrincipalExtensions.cs
+++ Server/Models/ClaimsPrincipalExtensions.cs
@@ -4,8 +4,6 @@ namespace WASMChat.Server.Models;
 
 public static class ClaimsPrincipalExtensions
 {
-	private const string NameClaim = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
-	
 	/// <summary>
 	/// Gets this <see cref="ClaimsPrincipal"/>s name that corresponds with
 	/// <see cref="ApplicationUser"/>s id.
@@ -13,5 +11,5 @@ public static class ClaimsPrincipalExtensions
 	/// <param name="principal"></param>
 	/// <returns></returns>
 	public static string? GetUserId(this ClaimsPrincipal principal)
-		=> principal.FindFirstValue(NameClaim);
+		=> principal.FindFirstValue(ClaimTypes.NameIdentifier);
 }
