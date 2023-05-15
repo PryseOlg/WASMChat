ed4adecac1d904e6ecd5c907bd687e302c4574b7
Игорь Бабин <igorbabin2002@gmail.com>
2023-05-15 17:34:52 +0500

    Frontend refactor, added users unique colors

--- Shared/Models/Chats/ChatUserModel.cs
+++ Shared/Models/Chats/ChatUserModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace WASMChat.Shared.Models.Chats;
 
-public class ChatUserModel
+public record ChatUserModel
 {
     public required int Id { get; init; }
     public required string UserName { get; init; }
